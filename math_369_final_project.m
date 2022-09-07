143ec1d67167aaf0df527e7373e289e51f5a3cec
davidaristoff <50423267+davidaristoff@users.noreply.github.com>
2022-09-06 20:14:08 -0600

    Add files via upload

--- math_369_final_project.m
+++ math_369_final_project.m
@@ -4,77 +4,101 @@ close all
 
 load Kingrynormalized.mat
 X = Kingrynorm;
+disp('find dimensions of data matrix')
+[n,m] = size(X)
+disp('check that data is centered, i.e. has mean zero')
+norm(mean(X,2))
+
+%plot the data matrix 
+
+% figure
+% imagesc(X)
+% colorbar
+% title('data matrix of mice infected with F. Tularensis','interpreter','latex')
+% xlabel('samples','interpreter','latex')
+% ylabel('gene expression','interpreter','latex')
+% set(gca,'ydir','normal')
+% set(gca,'FontSize',22)
 
 %compute SVD and plot singular values
 
-figure('Renderer', 'painters', 'Position', [100 100 1800 1000])
-imagesc(X')
-colorbar
-title('data matrix of mice infected with F. Tularensis (LVS and Schu4 strains)','interpreter','latex')
-ylabel('samples (from lung \& spleen of mice)','interpreter','latex')
-xlabel('gene expression','interpreter','latex')
-set(gca,'ydir','normal')
-set(gca,'FontSize',22)
+[U S V] = svd(X,0);    %take "thin" SVD of X
+sigmas = diag(S);
+figure
+plot(sigmas.^2)
 
+%check that singular values are square roots of eigenvalues of X^TX
 
-[U S V] = svd(X, 0);
-s = diag(S);
-% figure
-% plot(s)
-% xlabel('index')
-% ylabel('singular value')
+disp('check that singular values are square roots of eigenvalues of X^TX')
+lams = eig(X'*X);
+lams = sort(lams,'descend');
+norm(sigmas.^2-lams)
 
-control_lung = X(:,1:6);
-control_spleen = X(:,55:60);
-Schu4_lung = X(:,7:30);
-LVS_lung = X(:,31:54);
-Schu4_spleen = X(:,61:84);
-LVS_spleen = X(:,85:108);
+%check that columns of U are eigenvectors of XX^T
 
-U3 = U(:,1:3);    %3 principal directions
+disp('check that first column of U is an eigenvector of XX^T')
+u1 = U(:,1);       %pull first column of U
+lam1 = lams(1);    %pull first eigenvalue of XX^T
+norm(X*(X'*u1)-lam1*u1)    %this value should be "numerically zero"
+%now repeat for a few other columns of U
 
-%project data onto 3 principal directions.
-control_lung_3d = control_lung'*U3;
-control_spleen_3d = control_spleen'*U3;
-Schu4_lung_3d = Schu4_lung'*U3;
-LVS_lung_3d = LVS_lung'*U3;
-Schu4_spleen_3d = Schu4_spleen'*U3;
-LVS_spleen_3d = LVS_spleen'*U3;
+%now project X onto the span of the first three eigenvectors of XX^T
 
-figure('Renderer', 'painters', 'Position', [100 100 1800 1000])
-a = X'*U(:,1:3);
-plot3(control_lung_3d(:,1),control_lung_3d(:,2),control_lung_3d(:,3),'b+','linewidth',2)
+U_3d = U(:,1:3);    %pull the first three columns of U
+X_3d = U_3d'*X;     %project X onto the column span of U_3d
+
+%then plot the results. This is a 3d PCA plot
+
+figure
+plot3(X_3d(1,13),X_3d(2,13),X_3d(3,13),'sy','linewidth',40)
 hold on
-plot3(control_spleen_3d(:,1),control_spleen_3d(:,2),control_spleen_3d(:,3),'bd','linewidth',2)
-plot3(Schu4_lung_3d(:,1),Schu4_lung_3d(:,2),Schu4_lung_3d(:,3),'ro','linewidth',2)
-plot3(LVS_lung_3d(:,1),LVS_lung_3d(:,2),LVS_lung_3d(:,3),'gx','linewidth',2)
-plot3(Schu4_spleen_3d(:,1),Schu4_spleen_3d(:,2),Schu4_spleen_3d(:,3),'kv','linewidth',2)
-plot3(LVS_spleen_3d(:,1),LVS_spleen_3d(:,2),LVS_spleen_3d(:,3),'m^','linewidth',2)
-legend('control lung','control spleen', ...
+plot3(X_3d(1,1:6),X_3d(2,1:6),X_3d(3,1:6),'b+')
+plot3(X_3d(1,55:60),X_3d(2,55:60),X_3d(3,55:60),'bd')
+plot3(X_3d(1,7:30),X_3d(2,7:30),X_3d(3,7:30),'ro')
+plot3(X_3d(1,31:54),X_3d(2,31:54),X_3d(3,31:54),'gx')
+plot3(X_3d(1,61:84),X_3d(2,61:84),X_3d(3,61:84),'m^')
+plot3(X_3d(1,85:108),X_3d(2,85:108),X_3d(3,85:108),'kv')
+legend('highlighted sample','control lung','control spleen',...
     'Schu4 lung','LVS lung','Schu4 spleen','LVS spleen')
-title('PCA plot of gene expression in mice infected with F. Tularensis','interpreter','latex')
-xlabel('first principal axis','interpreter','latex')
-ylabel('second principal axis','interpreter','latex')
-zlabel('third principal axis','interpreter','latex')
-set(gca,'FontSize',22)
 
-%now project Schu4 lung and LVS lung onto control lung
+%now analyze the spleen data
+%project the Schu4 spleen data onto the span of the control spleen data
+%and analyze the associated "novelties"
+%then repeat for the LVS spleen data.
 
-% U = orth(control_lung);    %U = orthonormal basis for control lung
-% figure
-% hist(vecnorm(Schu4_lung - U*(U'*Schu4_lung)))
-% figure
-% hist(vecnorm(LVS_lung - U*(U'*LVS_lung)))
-% 
-% %now project Schu4 spleen and LVS spleen onto control spleen
-% 
-% U = orth(control_spleen);    %U = orthonormal basis for control lung
-% figure
-% hist(vecnorm(Schu4_spleen - U*(U'*Schu4_spleen)))
-% figure
-% hist(vecnorm(LVS_spleen - U*(U'*LVS_spleen)))
+control_spleen = X(:,55:60);  %pull uninfected spleen samples
+Schu4_spleen = X(:,61:84);    %pull Schu4 infected spleen samples
+LVS_spleen = X(:,85:108);     %pull LVS infected spleen samples
 
-%COMMENT ON WHAT NOVELTIES ARE LARGER. WHAT DOES THIS MEAN QUALITATIVELY
+U = orth(control_spleen);
+a = 0;
+for i=1:24
+    v = Schu4_spleen(:,i);      %v = ith Schu4 spleen sample
+    proj_Uv = U*(U'*v);         %proj_Uv = projection of v onto Im(U)
+    norm(v - proj_Uv);          %display norm of v-proj_Uv
+    a = a + norm(v - proj_Uv);
+end
+a = a/24
+
+b = 0;
+for i=1:24
+    v = LVS_spleen(:,i);        %v = ith Schu4 spleen sample
+    proj_Uv = U*(U'*v);         %proj_Uv = projection of v onto Im(U)
+    norm(v - proj_Uv);          %display norm of v-proj_Uv
+    b = b + norm(v - proj_Uv);
+end
+b = b/24
+
+c = 0;
+for i=1:6
+    samples = [1:i-1,i+1:6]; 
+    U = orth(control_spleen(:,samples));
+    v = control_spleen(:,i);
+    proj_Uv = U*(U'*v);
+    norm(v-proj_Uv);
+    c = c + norm(v-proj_Uv);
+end
+c = c/6
 
 %now do Fisher discriminant analysis on the data
 
@@ -109,11 +133,11 @@ for i=7:54
     Cp(i-6) = w'*A(:,i);
 end
 
-figure('Renderer', 'painters', 'Position', [100 100 1800 1000])
-plot(Cm,zeros(6,1),'ob','linewidth',2,'markersize',10)
-hold on
-plot(Cp,zeros(48,1),'xr','linewidth',2,'markersize',10)
-title('Fisher discriminant analysis of gene expression in mice infected with F. Tularensis','interpreter','latex')
-legend('control spleen','infected spleen')
-set(gca,'FontSize',22)
+% figure('Renderer', 'painters', 'Position', [100 100 1800 1000])
+% plot(Cm,zeros(6,1),'ob','linewidth',2)
+% hold on
+% plot(Cp,zeros(48,1),'xr','linewidth',2)
+% title('Fisher discriminant analysis of gene expression in mice infected with F. Tularensis','interpreter','latex')
+% legend('control spleen','infected spleen')
+% set(gca,'FontSize',22)
 
